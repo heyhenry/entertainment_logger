122f0d4ef135e52774253246ed14a7ff16973530
heyhenry <henryyy3@hotmail.com>
2024-02-21 22:27:49 +0800

    Implemented basic menu

---
+++ main.py
@@ -0,0 +1,15 @@
+def menu():
+
+    print("--- Menu ---")
+    print("1. Update anime")
+    print("2. Show anime list")
+    print("3. Exit")
+    print("--- [  ] ---")
+    
+    choice = int(input("Enter 1 - 3: "))
+
+def main():
+    print(menu())
+
+if __name__ == "__main__":
+    main()
