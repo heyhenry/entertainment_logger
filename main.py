300817351e64bbf3eba78b9be1dd070a0f75b9d2
heyhenry <henryyy3@hotmail.com>
2024-02-21 22:39:30 +0800

    implemented initial anime list display (only the titles atm).

--- main.py
+++ main.py
@@ -8,8 +8,35 @@ def menu():
     
     choice = int(input("Enter 1 - 3: "))
 
+    if choice == 2:
+        display_anime_list("anime.records")
+
+def display_anime_list(filename : str):
+
+    with open(filename, 'r') as file:
+        
+        read_content = file.read()
+
+        lines = read_content.splitlines()
+
+        categories = []
+
+        title_bar = ''
+
+        for i in lines[0].split(','):
+            categories.append(i)
+
+        for i in range(len(categories)):
+            if i != len(categories) - 1:
+                title_bar += (categories[i] + ' | ')
+            else:
+                title_bar += categories[i]
+
+        print(title_bar)
+
 def main():
     print(menu())
+    # print(display_anime_list("anime.records")) # test case
 
 if __name__ == "__main__":
     main()
